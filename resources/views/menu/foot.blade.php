2fa390fbb63a812deafcb84fe2215414d449e5de
Pornnapa-Panyadee <pornnapa.van@gmail.com>
2025-05-26 14:07:13 +0700

    weir chiang mai 2025

---
+++ resources/views/menu/foot.blade.php
@@ -0,0 +1,20 @@
+<div class="footer" style="background-color:#fff">
+    <div >
+        <br>
+        <center><h4>โครงการพัฒนาระบบสารสนเทศการตรวจประเมินสภาพฝาย
+            <br>และการบริหารจัดการพื้นที่เสี่ยงภัยแล้งและน้ำท่วมในจังหวัดเชียงใหม่</h4></center>
+        <table align="center" width=60%>
+            <tr >
+               <td width=50% align="right">
+                โดย<br>
+                องค์การบริหารส่วนจังหวัดเชียงใหม่ <br> มหาวิทยาลัยเชียงใหม่ 
+               </td>
+               <td width=2% align="right"></td>
+               <td width=50% align="left">
+                <a href="https://www.chiangmaipao.go.th/"> <img src="{{ asset('images/footer/cm.png') }}" width="15%"></a>
+                <a href="https://eng.cmu.ac.th/"> <img src="{{ asset('images/footer/cmu.png') }}" width="15%"></a>
+            </td>
+          </tr>
+        </table>
+    </div>
+</div
